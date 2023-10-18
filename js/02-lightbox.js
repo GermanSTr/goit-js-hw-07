6905fd9e911de2036655bdab1c1d4385bfd3dd61
GermanSTr <gershev1994@gmail.com>
2023-10-18 18:20:34 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+function renderImg() {
+  const galleryHTML = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`;
+  });
+  galleryEl.innerHTML = galleryHTML.join("");
+}
+renderImg();
+
+var lightbox = new SimpleLightbox(".gallery .gallery__link", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
